b67e6962d17dcc3b238747c475a2b3dbfae08c5f
Abhishek <abhisheks@pearlorganisation.com>
2026-02-23 15:02:25 +0530

    'filelimit'

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -21,7 +21,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
     storage,
-    limits: { fileSize: 50 * 1024 * 1024 } // 50MB
+    limits: { fileSize: 100 * 1024 * 1024 } // 100MB
 });
 
 module.exports = upload;
